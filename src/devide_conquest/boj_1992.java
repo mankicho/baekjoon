205ae2efc6fee5aaba24842a2b5f1a74f704aaa2
manki <skxz123@gmail.com>
2021-05-15 16:30:46 +0900

    2021-05-15 devide_conquest(1992,2630)

--- src/devide_conquest/boj_1992.java
+++ src/devide_conquest/boj_1992.java
@@ -4,9 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class boj_2630 {
-    static int white = 0;
-    static int blue = 0;
+public class boj_1992 {
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,42 +14,41 @@ public class boj_2630 {
 
         int[][] arr = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
         }
 
         function(arr, 0, n, 0, n);
 
-
+        System.out.println(sb.toString());
 
     }
 
-    static void function(int[][] arr, int startRow, int endRow, int startCol, int endCol) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int value = arr[startRow][startCol];
+    static void function(int[][] arr, int startRow, int endRow, int startCol, int endCol) {
+        int val = arr[startRow][startCol];
         boolean needDevide = false;
-        LOOP:
+        OUTER_LOOP:
         for (int i = startRow; i < endRow; i++) {
             for (int j = startCol; j < endCol; j++) {
-//                br.readLine();
-                if (arr[i][j] != value) {
+                if (arr[i][j] != val) {
                     needDevide = true;
-                    break LOOP;
+                    break OUTER_LOOP;
                 }
             }
         }
 
         if (needDevide) {
+            sb.append("(");
             function(arr, startRow, (startRow + endRow) / 2, startCol, (startCol + endCol) / 2);
-            function(arr, (startRow + endRow) / 2, endRow, startCol, (startCol + endCol) / 2);
             function(arr, startRow, (startRow + endRow) / 2, (startCol + endCol) / 2, endCol);
+            function(arr, (startRow + endRow) / 2, endRow, startCol, (startCol + endCol) / 2);
             function(arr, (startRow + endRow) / 2, endRow, (startCol + endCol) / 2, endCol);
+            sb.append(")");
         } else {
-            if (value == 0) {
-                white++;
+            if (val == 0) {
+                sb.append(0);
             } else {
-                blue++;
+                sb.append(1);
             }
         }
     }
