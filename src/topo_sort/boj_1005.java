e0b43a2b8783c6ecfb4a9f80d2aebf5cf135802d
manki <skxz123@gmail.com>
2021-06-03 20:40:06 +0900

    위상정렬 1005

--- src/topo_sort/boj_1005.java
+++ src/topo_sort/boj_1005.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class boj_1005 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
