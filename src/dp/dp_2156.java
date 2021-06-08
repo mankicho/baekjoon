c8a6fd388d0ddfe3566ea3007aab006e5d2166ee
mankicho <skxz123@gmail.com>
2021-06-08 15:53:20 +0900

    dp 2156

---
+++ src/dp/dp_2156.java
@@ -0,0 +1,38 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class dp_2156 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        if (n == 1) {
+            System.out.println(arr[1]);
+            return;
+        }
+
+        if (n == 2) {
+            System.out.println(arr[1] + arr[2]);
+            return;
+        }
+        int[] dp = new int[n + 1];
+
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]);
+            dp[i] = Math.max(dp[i - 1], dp[i]);
+        }
+        System.out.println(dp[n]);
+    }
+
+}
